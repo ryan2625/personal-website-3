d36b6b26631cb58395f06b5036da0a93ac92c14a
ryan2625 <ryanfreas19@augustana.edu>
2023-11-03 14:42:19 -0500

    Adding SVGs and JQUERY logic

--- data.js
+++ data.js
@@ -1,44 +1,30 @@
-import nightHawk from './images/nightHawk.png';
-import flavor from './images/flavors.png';
-import boxing from './images/boxing.png';
-import nexus from './images/nexus.png';
-import personal from './images/personal.png';
-
 export const data = [
     {
-        image : nightHawk, 
-        title : "Nighthawk Fitness", 
-        tech : "React, CSS, Javascript, Firebase ", 
-        description : "A full-stack fitness app that can track your weight, create and log your workouts, and track your calories. This app is fully responsive and also includes user authentication and supports CRUD operations. ", 
-        link : "https://fitness-dev-2.web.app/", 
-        github : "https://github.com/AugustanaCSC490Spring23/Nighthawk-Fitness"},
-    {
-        image : flavor,
+        image : './images/flavors.png',
         title : "Flavor Store",
         tech : "MERN Stack, CSS, React Bootstrap",
         description : "A fully responsive app that supports CRUD operations and dynamically renders items from the backend. Mindful of SEO and accessibility, this website was made for National Louis University's coding challenge.",
         link : "https://mern-flavor-site.onrender.com/",
         github : "https://github.com/ryan2625/NLU-flavor-app"},
     {
-        image : boxing,
+        image : './images/boxing.png',
         title : "Cross-City Boxing",
         tech : "Javascript, HTML, CSS",
         description : "A fully responsive react app created based on a Figma design.",
         link : "https://cross-city-boxing.vercel.app/",
         github : "https://github.com/ryan2625/cross-city-boxing"},
     {
-        image : nexus,
+        image : './images/nexus.png',
         title : "Nexus Retailers",
         tech : "React, CSS, Javascript, Bootstrap",
         description : "A fully responsive react app created using bootstrap and the Intersection Observer API.",
         link : "https://nexus-cars.vercel.app/",
         github : "https://github.com/ryan2625/nexus-Cars"},
     {
-        image : personal,
+        image : './images/personal.png',
         title : "Personal Website",
         tech : "React, Javascript, CSS",
         description : "One of my first web development projects. Creating this website was a special experience and my first attempt at building responsive websites from scratch. If you would like to get to know more about me and see more of my projects and research, visit here! ",
         link : "https://ryan-dev.vercel.app/",
         github : "https://github.com/ryan2625/personalWebsite"},
-    
 ]
--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+import { data } from "./data.js";
+
 $(document).ready(function() {
-    $(".dark").text("This text is updated using jQuery!");
+
+    data.forEach((element) => {
+        var img = $('<img>').attr('src', element.image);
+        var individual = $('<div>').addClass('individual');
+        var divider = $('<div>').addClass('portfolio-divider');
+        var desc = $('<div>').addClass('descriptions');
+        var titles = $('<div>').addClass('titles');
+        var h1 = $('<h1>').text(element.title);
+        var h2 = $('<h2>').text(element.tech);
+        var p = $('<p>').text(element.description);
+        var buttons = $('<div>').addClass('buttons');
+        var a1 = $('<a>').attr('href', element.link).text('Live App');
+        var a2 = $('<a>').attr('href', element.github).text('Github');
+
+        individual.append(img)
+        individual.append(desc)
+
+        desc.append(titles)
+        desc.append(p)
+        desc.append(buttons)
+
+        titles.append(h1)
+        titles.append(h2)
+
+        buttons.append(a1)
+        buttons.append(a2)
+
+        $(".portfolio-display").append(individual);
+        $(".portfolio-display").append(divider)
+        
+    });
+
   });
