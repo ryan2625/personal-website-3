c8e8a04b1cca319d717f99103308777c325b92f6
ryan2625 <ryanfreas19@augustana.edu>
2023-11-04 08:20:24 -0500

    Adding intersection observer API

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ import { data } from "./data.js";
 
 $(document).ready(function() {
 
-    data.forEach((element) => {
-        var img = $('<img>').attr('src', element.image);
-        var individual = $('<div>').addClass('individual');
+    data.forEach((element, index) => {
+        var img = $('<img>').attr('src', element.image)
+        var individual = $('<div>').addClass('individual').addClass("hidden");
         var divider = $('<div>').addClass('portfolio-divider');
         var desc = $('<div>').addClass('descriptions');
         var titles = $('<div>').addClass('titles');
@@ -14,7 +14,7 @@ $(document).ready(function() {
         var buttons = $('<div>').addClass('buttons');
         var a1 = $('<a>').attr('href', element.link).text('Live App');
         var a2 = $('<a>').attr('href', element.github).text('Github');
-
+        individual.attr('id', index)
         individual.append(img)
         individual.append(desc)
 
@@ -30,7 +30,30 @@ $(document).ready(function() {
 
         $(".portfolio-display").append(individual);
         $(".portfolio-display").append(divider)
-        
+
     });
 
+    var img2 = $('<img>').attr('src', "./images/blobProp3.svg").attr('id', 'blob');
+    var img3 = $('<img>').attr('src', "./images/blobProp2.svg").attr('id', 'blob2');
+    var img4 = $('<img>').attr('src', "./images/blobProp4.svg").attr('id', 'blob4');
+
+    $("#0").append(img2)
+    $("#1").append(img3)
+    $("#2").append(img4)
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, {
+        rootMargin: "0px 0px -250px 0px" 
+    });
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+
+    hiddenElements.forEach(element => {
+        observer.observe(element);
+    });
   });
